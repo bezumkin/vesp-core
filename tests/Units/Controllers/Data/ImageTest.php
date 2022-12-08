0b02d941c1c627a66189360e418277901b0c3f44
bezumkin <bezumkin@yandex.ru>
2022-12-08 11:48:49 +0700

    feat: ability of extension for special processing of file in data\image controller

--- tests/Units/Controllers/Data/ImageTest.php
+++ tests/Units/Controllers/Data/ImageTest.php
@@ -14,6 +14,7 @@ class ImageTest extends TestCase
     // @codingStandardsIgnoreStart
     protected const PNG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==';
     protected const GIF = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+    protected const SVG = 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB3aWR0aD0iMSIgaGVpZ2h0PSIxIiB2aWV3Qm94PSIwIDAgMSAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciLz4K';
     protected const TXT = 'data:text/plain;base64,dGVzdA==';
 
     // @codingStandardsIgnoreEnd
@@ -53,6 +54,18 @@ class ImageTest extends TestCase
     {
         $file = new File();
         $file->uploadFile(self::GIF, ['name' => 'test.gif']);
+
+        $request = $this->createRequest('GET', self::URI, ['id' => $file->id]);
+        $response = $this->app->handle($request);
+        $file->delete();
+
+        self::assertEquals(200, $response->getStatusCode(), $response->getBody());
+    }
+
+    public function testGetSvgSuccess(): void
+    {
+        $file = new File();
+        $file->uploadFile(self::SVG, ['name' => 'test.svg']);
 
         $request = $this->createRequest('GET', self::URI, ['id' => $file->id]);
         $response = $this->app->handle($request);
