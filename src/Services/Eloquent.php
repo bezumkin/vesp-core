817387be21ee1c93cbc3c9b42e65409efb7cf5e2
bezumkin <bezumkin@yandex.ru>
2023-04-07 12:11:49 +0700

    feat: use prefix for table indexes by default

--- src/Services/Eloquent.php
+++ src/Services/Eloquent.php
@@ -9,23 +9,27 @@ use Illuminate\Events\Dispatcher;
 
 class Eloquent extends Manager
 {
-    public function __construct()
+    public function __construct(array $config = [])
     {
         parent::__construct();
 
         $this->addConnection(
-            [
-                'driver' => getenv('DB_DRIVER'),
-                'host' => getenv('DB_HOST'),
-                'port' => getenv('DB_PORT'),
-                'prefix' => getenv('DB_PREFIX'),
-                'database' => getenv('DB_DATABASE'),
-                'username' => getenv('DB_USERNAME'),
-                'password' => getenv('DB_PASSWORD'),
-                'charset' => getenv('DB_CHARSET'),
-                'collation' => getenv('DB_COLLATION'),
-                'foreign_key_constraints' => getenv('DB_FOREIGN_KEYS'),
-            ]
+            array_merge(
+                [
+                    'driver' => getenv('DB_DRIVER'),
+                    'host' => getenv('DB_HOST'),
+                    'port' => getenv('DB_PORT'),
+                    'prefix' => getenv('DB_PREFIX'),
+                    'database' => getenv('DB_DATABASE'),
+                    'username' => getenv('DB_USERNAME'),
+                    'password' => getenv('DB_PASSWORD'),
+                    'charset' => getenv('DB_CHARSET'),
+                    'collation' => getenv('DB_COLLATION'),
+                    'foreign_key_constraints' => getenv('DB_FOREIGN_KEYS'),
+                    'prefix_indexes' => getenv('DB_PREFIX_INDEXES'),
+                ],
+                $config
+            )
         );
         $this->setEventDispatcher(new Dispatcher());
         $this->setAsGlobal();
